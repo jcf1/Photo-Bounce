21da1cb4ebf21bbcc37b17c0a182c6ddb8d6f23d
jcf1 <john.c.freeman17@gmail.com>
2023-03-20 16:43:09 -0400

    Added ability to upload and display image

---
+++ src/components/PhotoBounce.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    
+    export let width: Number = 64;
+    export let height: Number = 36;
+    export let bgColor: String = "black"
+    export let bgPhotos: FileList;
+
+
+    let bgIndex = 0;
+
+    $: if(bgPhotos) {
+        console.log('RENDER')
+        bgPhotos = bgPhotos
+    }
+
+    function getBGPhoto(): string {
+        console.log(bgPhotos)
+        if(bgPhotos) {
+            return URL.createObjectURL(bgPhotos[0]);
+        }
+        return "data:,";
+    }
+
+    function getStyle(): string {
+        return "height:"+height+"vh; width:"+width+"vh; background-color:"+bgColor+";"
+    }
+
+    export function run() {
+        console.log('PB')
+        console.log(bgPhotos)
+    }
+</script>
+
+<div class="flex items-center justify-center" style={getStyle()}>
+    {#if bgPhotos}
+        <img class="w-full h-full" src={URL.createObjectURL(bgPhotos[bgIndex])} alt=""/>
+    {/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import PhotoBounce from "../components/PhotoBounce.svelte";
+
+    let photobounce: PhotoBounce;
+
+    let bgFileInput: HTMLInputElement;
+    let bgPhotos: FileList;
+
+    let refresh: boolean = true;
+
+    function uploadBackgroundPhotos() {
+        bgFileInput.click();
+        bgPhotos = bgPhotos;
+    }
+
+    function run() {
+        console.log('RUN');
+        console.log(bgPhotos);
+        photobounce.run();
+        refresh != refresh;
+    }
+
+</script>
+
+<div class="grid gap-1 justify-center justify-items-center items-center h-screen w-screen">
+    <div class="text-6xl">Photo Bounce</div>
+    <PhotoBounce 
+        bind:this={photobounce}
+        width={64}
+        height={36}
+        bgColor={"black"}
+        {bgPhotos}
+    />
+    <input
+        class="hidden"
+        accept="image/png, image/jpeg, image/jpg"
+        multiple
+        type="file"
+        bind:this={bgFileInput}
+        bind:files={bgPhotos}
+    />
+    <div>
+        <button on:click={() => {uploadBackgroundPhotos()}}>Upload Background Images</button>
+        <div class="text-lg">{bgPhotos ? bgPhotos.length : 0} Selected</div>
+    </div>
+    <button on:click={()=> {run()}} type=submit>RUN</button>
+</div>
