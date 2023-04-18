a765cc8c3b439c5f2537099c0441f6c53355f5c6
jcf1 <john.c.freeman17@gmail.com>
2023-04-18 01:12:22 -0400

    Fixed advanced settings to reflect more accurate values

--- src/components/settings/AdvancedSettings.svelte
+++ src/components/settings/AdvancedSettings.svelte
@@ -3,28 +3,36 @@
     export let startY: number;
     export let startDir: number;
     export let bounceAngle: number;
+
+    let xValue = startX;
+    let yValue = 100 - startY;
+    let startValue = 360 - startDir;
+    
+    $: startX = xValue;
+    $: startY = 100 - yValue;
+    $: startDir = 360 - startValue;
 </script>
 
 <div class="justify-center">
 
     <div class="flex flex-row justify-center">
         <label class="pr-1 text-lg">
-            Start bounce image at {startX}% width:
-            <input class="border border-black" type="range" max="100" min="0" bind:value={startX}/>
+            Start bounce image at {xValue}% width:
+            <input class="border border-black" type="range" max="100" min="0" bind:value={xValue}/>
         </label>
     </div>
 
     <div class="flex flex-row justify-center">
         <label class="pr-1 text-lg">
-            Start bounce image at {startY}% height:
-            <input class="border border-black" type="range" max="100" min="0" bind:value={startY}/>
+            Start bounce image at {yValue}% height:
+            <input class="border border-black" type="range" max="100" min="0" bind:value={yValue}/>
         </label>
     </div>
 
     <div class="flex flex-row justify-center">
         <label class="pr-1 text-lg">
-            Start Bounce direction at {startDir}&#176;:
-            <input class="border border-black" type="range" max="360" min="1" bind:value={startDir}/>
+            Start Bounce direction at {startValue}&#176;:
+            <input class="border border-black" type="range" max="359" min="0" bind:value={startValue}/>
         </label>
     </div>
 
